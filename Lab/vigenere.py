5b32e1011ffbcd5f335e09a4553506a78b71e7a1
-v <mark.selyaeff@yandex.ru>
2016-09-21 20:21:05 +0300

    length_equal function added

--- Lab/vigenere.py
+++ Lab/vigenere.py
@@ -1,14 +1,24 @@
-def encrypt_vigenere(plaintext, keyword):
-	ciphertext = '' # Обнуляем строку
-	
-	# Подгонка ключа под размер входного слова
-	if len(plaintext) > len(keyword):
-		modulo = len(plaintext) % len(keyword)
-		keyword = keyword * int((len(plaintext) / len(keyword)))
+def length_equal(s, keyword): 
+	if len(s) > len(keyword):
+		modulo = len(s) % len(keyword)
+		keyword = keyword * int((len(s) / len(keyword)))
 		if modulo:
 			keyword = keyword + keyword[:modulo]
 	else:
-		keyword = keyword[:len(plaintext)]
+		keyword = keyword[:len(s)]
+	return keyword
+
+def encrypt_vigenere(plaintext, keyword):
+	ciphertext = '' # Обнуляем строку
+	keyword = length_equal(plaintext, keyword)
+	# Подгонка ключа под размер входного слова
+	# if len(plaintext) > len(keyword):
+	# 	modulo = len(plaintext) % len(keyword)
+	# 	keyword = keyword * int((len(plaintext) / len(keyword)))
+	# 	if modulo:
+	# 		keyword = keyword + keyword[:modulo]
+	# else:
+	# 	keyword = keyword[:len(plaintext)]
 
 	# Получаем зашифрованное слово	
 	for i in range(len(plaintext)):
