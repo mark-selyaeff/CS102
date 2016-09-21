8aa121dd7b3d1e6f944350fd6f072ce28b300e7a
-v <mark.selyaeff@yandex.ru>
2016-09-21 22:46:54 +0300

    Decrypt function added

--- Lab/vigenere.py
+++ Lab/vigenere.py
@@ -1,4 +1,4 @@
-def length_equal(s, keyword): 
+def length_equal(s, keyword): # Уравнивание длины строк
 	if len(s) > len(keyword):
 		modulo = len(s) % len(keyword)
 		keyword = keyword * int((len(s) / len(keyword)))
@@ -11,16 +11,6 @@ def length_equal(s, keyword):
 def encrypt_vigenere(plaintext, keyword):
 	ciphertext = '' # Обнуляем строку
 	keyword = length_equal(plaintext, keyword)
-	# Подгонка ключа под размер входного слова
-	# if len(plaintext) > len(keyword):
-	# 	modulo = len(plaintext) % len(keyword)
-	# 	keyword = keyword * int((len(plaintext) / len(keyword)))
-	# 	if modulo:
-	# 		keyword = keyword + keyword[:modulo]
-	# else:
-	# 	keyword = keyword[:len(plaintext)]
-
-	# Получаем зашифрованное слово	
 	for i in range(len(plaintext)):
 		if (ord(plaintext[i].upper()) + ord(keyword[i].upper()) - 65) <= 90:
 			ciphertext += chr(ord(plaintext[i].upper()) + (ord(keyword[i].upper()) - 65))
@@ -28,8 +18,19 @@ def encrypt_vigenere(plaintext, keyword):
 			ciphertext +=  chr(ord(keyword[i].upper()) - 91 + ord(plaintext[i].upper()))
 	return ciphertext
 
-plaintext = input('Enter a plaintext: ')
+def decrypt_vigenere(ciphertext, keyword):
+	plaintext = ''
+	keyword = length_equal(ciphertext, keyword)
+	for i in range(len(ciphertext)):
+		if (ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65)) >= 65:
+			plaintext += chr(ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65))
+		else:
+			plaintext +=  chr(ord(ciphertext[i].upper()) - ord(keyword[i].upper()) + 91)	
+	return plaintext
+
+
+a = input('Enter a plaintext: ')
 keyword = input('Enter a key: ')
-print(encrypt_vigenere(plaintext, keyword))
+print(decrypt_vigenere(a, keyword))
 
 
