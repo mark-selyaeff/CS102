1a551f162f47a258a0b06067ce5828490a743eba
-v <mark.selyaeff@yandex.ru>
2016-09-25 14:15:48 +0300

    Added generate_keypair function and changed is_prime func

--- Lab/rsa.py
+++ Lab/rsa.py
@@ -3,8 +3,27 @@ import math
 def is_prime(n):
 	n = int(n)
 	is_prime = True
-	for i in range (3, int(math.sqrt(n))):
-		if n % i == 0 and is_prime:
-			is_prime = False
+	if n % 2 != 0:
+		for i in range (3, int(math.sqrt(n))+1, 2):
+			if n % i == 0 and is_prime:
+				is_prime = False
+	else: 
+		is_prime = False
 	return is_prime
-print(is_prime(input("Enter a number: ")))
+
+def generate_keypair(p, q):
+	if not (is_prime(p) and is_prime(q)):
+		raise ValueError('Both numbers must be prime.')
+	elif p == q:
+		raise ValueError('p and q cannot be equal')
+
+generate_keypair(input('Enter a first prime: '), input('Enter a second prime: '))
+
+ 
+
+
+
+
+
+
+
