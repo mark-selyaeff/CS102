7e04e7a6e711dd70746708e488edbc983f93b241
-v <mark.selyaeff@yandex.ru>
2016-09-26 13:27:31 +0300

    Style based on PEP8

--- Lab/caesar.py
+++ Lab/caesar.py
@@ -1,24 +1,25 @@
 # Шифровка
 def encrypt_caesar(plaintext):
-	plaintext = str(plaintext)
-	ciphertext = ""
-	for i in range (len(plaintext)):
-		if (88 <= ord(plaintext[i]) <= 90) or (120 <= ord(plaintext[i]) <= 122):
-			ciphertext += chr(ord(plaintext[i]) - 23)
-		else:
-			ciphertext += chr(ord(plaintext[i]) + 3)
-	return ciphertext
+    plaintext = str(plaintext)
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        if (88 <= ord(plaintext[i]) <= 90) or (120 <= ord(plaintext[i]) <= 122):
+            ciphertext += chr(ord(plaintext[i]) - 23)
+        else:
+            ciphertext += chr(ord(plaintext[i]) + 3)
+    return ciphertext
+
 
 # Расшифровка
 def decrypt_caesar(ciphertext):
-	ciphertext = str(ciphertext)
-	plaintext = ""
-	for i in range (len(ciphertext)):
-		if (65 <= ord(ciphertext[i]) <= 67) or (97 <= ord(ciphertext[i]) <= 99):
-			plaintext += chr(ord(ciphertext[i]) + 23)
-		else:
-			plaintext += chr(ord(ciphertext[i]) - 3)
-	return plaintext
+    ciphertext = str(ciphertext)
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        if (65 <= ord(ciphertext[i]) <= 67) or (97 <= ord(ciphertext[i]) <= 99):
+            plaintext += chr(ord(ciphertext[i]) + 23)
+        else:
+            plaintext += chr(ord(ciphertext[i]) - 3)
+    return plaintext
 
 
 print(encrypt_caesar(input('Enter a word:')))
--- Lab/vigenere.py
+++ Lab/vigenere.py
@@ -1,36 +1,36 @@
-def length_equal(s, keyword): # Уравнивание длины строк
-	if len(s) > len(keyword):
-		modulo = len(s) % len(keyword)
-		keyword = keyword * int((len(s) / len(keyword)))
-		if modulo:
-			keyword = keyword + keyword[:modulo]
-	else:
-		keyword = keyword[:len(s)]
-	return keyword
+def length_equal(s, keyword):  # Уравнивание длины строк
+    if len(s) > len(keyword):
+        modulo = len(s) % len(keyword)
+        keyword = keyword * int((len(s) / len(keyword)))
+        if modulo:
+            keyword = keyword + keyword[:modulo]
+    else:
+        keyword = keyword[:len(s)]
+    return keyword
+
 
 def encrypt_vigenere(plaintext, keyword):
-	ciphertext = '' # Обнуляем строку
-	keyword = length_equal(plaintext, keyword)
-	for i in range(len(plaintext)):
-		if (ord(plaintext[i].upper()) + ord(keyword[i].upper()) - 65) <= 90:
-			ciphertext += chr(ord(plaintext[i].upper()) + (ord(keyword[i].upper()) - 65))
-		else:
-			ciphertext +=  chr(ord(keyword[i].upper()) - 91 + ord(plaintext[i].upper()))
-	return ciphertext
+    ciphertext = ''  # Обнуляем строку
+    keyword = length_equal(plaintext, keyword)
+    for i in range(len(plaintext)):
+        if (ord(plaintext[i].upper()) + ord(keyword[i].upper()) - 65) <= 90:
+            ciphertext += chr(ord(plaintext[i].upper()) + (ord(keyword[i].upper()) - 65))
+        else:
+            ciphertext += chr(ord(keyword[i].upper()) - 91 + ord(plaintext[i].upper()))
+    return ciphertext
+
 
 def decrypt_vigenere(ciphertext, keyword):
-	plaintext = ''
-	keyword = length_equal(ciphertext, keyword)
-	for i in range(len(ciphertext)):
-		if (ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65)) >= 65:
-			plaintext += chr(ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65))
-		else:
-			plaintext +=  chr(ord(ciphertext[i].upper()) - ord(keyword[i].upper()) + 91)	
-	return plaintext
+    plaintext = ''
+    keyword = length_equal(ciphertext, keyword)
+    for i in range(len(ciphertext)):
+        if (ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65)) >= 65:
+            plaintext += chr(ord(ciphertext[i].upper()) - (ord(keyword[i].upper()) - 65))
+        else:
+            plaintext += chr(ord(ciphertext[i].upper()) - ord(keyword[i].upper()) + 91)
+    return plaintext
 
 
 a = input('Enter a plaintext: ')
 keyword = input('Enter a key: ')
 print(decrypt_vigenere(a, keyword))
-
-
