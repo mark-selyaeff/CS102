32ad8784b61e7779023d6698fefa04b3e8ea07da
-v <mark.selyaeff@yandex.ru>
2016-10-03 17:11:49 +0300

    Added main info

--- Lab/rsa.py
+++ Lab/rsa.py
@@ -12,6 +12,8 @@ def is_prime(n):
         is_prime = False
     return is_prime
 
+def is_prime_1(val):
+    return [x for x in range(2,val+1) if all((x%y for y in range(2,1+int(x**0.5))))]
 
 def gcd(a, b):
     a = int(a)
@@ -46,8 +48,40 @@ def generate_keypair(p, q):
             break
     x, y, d = gcd_extended(phi, e)
     d = d % phi
-    print(e, n)
-    print(d, n)
+    return ((e, n), (d, n))
 
 
-generate_keypair(input('Enter a first prime: '), input('Enter a second prime: '))
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
