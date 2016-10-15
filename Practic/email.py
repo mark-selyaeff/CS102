2b9de9fc3863184cafb41cdb4346c4857e229b8c
-v <mark.selyaeff@yandex.ru>
2016-10-15 15:50:23 +0300

    email practic

--- Practic/email.py
+++ Practic/email.py
@@ -8,7 +8,7 @@ print(emails_stripped)
 print(','.join(emails_stripped))
 f.close()
 
-''' with open('emails.txt') as f: 
+''' with open('emails.txt') as f:
 		emails_lst = f.readlines
 	emails_stripped = [email.(strip() for email in emails_lst]
 	filter(is_function, a_list) — профильтровать список по функции (она возвращает 0 или 1 ) '''
